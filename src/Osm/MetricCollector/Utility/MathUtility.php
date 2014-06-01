4a87d6a32bf20b5cada52a9477bce42a7f2eec7d
Osman Ungur <osmanungur@gmail.com>
2014-06-01 16:52:45 +0300

    Added MathUtility class and calculatePercentage method

---
+++ src/Osm/MetricCollector/Utility/MathUtility.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Osm\MetricCollector\Utility;
+
+/**
+ * Provides various utility about calculations.
+ */
+class MathUtility
+{
+
+    /**
+     * Calculates percentages of given array values.
+     *
+     * @param  array $values
+     * @return array
+     */
+    public static function calculatePercentages(array $values)
+    {
+        $total = array_sum($values);
+
+        return array_map(
+            function ($value) use ($total) {
+                return ($value / $total) * 100;
+            },
+            $values
+        );
+    }
+
+}
