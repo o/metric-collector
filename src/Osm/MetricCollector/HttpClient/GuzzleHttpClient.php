6b7d32e009843e6c8f6e82caded40dbc0168edc2
Osman Ungur <osmanungur@gmail.com>
2014-06-25 15:58:25 +0300

    Changed interface of HttpClient for more flexibility Implemented methods of GuzzleHttpClient

--- src/Osm/MetricCollector/HttpClient/GuzzleHttpClient.php
+++ src/Osm/MetricCollector/HttpClient/GuzzleHttpClient.php
@@ -19,6 +19,48 @@ class GuzzleHttpClient implements HttpClientInterface
     private $httpClient;
 
     /**
+     *
+     * @var array
+     */
+    private $authentication;
+
+    /**
+     *
+     * @var array
+     */
+    private $certificate;
+
+    /**
+     *
+     * @var boolean|string
+     */
+    private $verifyCertificate;
+
+    /**
+     *
+     * @var float
+     */
+    private $connectTimeout = 2;
+
+    /**
+     *
+     * @var float
+     */
+    private $timeout = 5;
+
+    /**
+     *
+     * @var array
+     */
+    private $headers;
+
+    /**
+     *
+     * @var string
+     */
+    private $proxy;
+
+    /**
      * Initializes GuzzleHttpClient
      *
      * @param Client $httpClient
@@ -31,17 +73,93 @@ class GuzzleHttpClient implements HttpClientInterface
     /**
      * {@inheritdoc}
      */
-    public function send($url, array $headers = array())
+    public function send($url)
     {
+        $options = array(
+            'headers' => $this->headers,
+            'auth' => $this->authentication,
+            'timeout' => $this->timeout,
+            'connect_timeout' => $this->connectTimeout,
+            'verify' => $this->verifyCertificate,
+            'cert' => $this->certificate,
+            'proxy' => $this->proxy
+        );
+
         try {
-            return $this->httpClient->get(
-                $url,
-                array(
-                    'headers' => $headers
-                )
-            )->getBody(true);
+            return $this->httpClient->get($url, $options)->getBody(true);
         } catch (TransferException $exception) {
             throw new HttpResponseException($exception->getMessage(), $exception->getCode());
         }
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAuthentication(array $authentication)
+    {
+        $this->authentication = $authentication;
+
+        return this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCertificate($path, $password = null)
+    {
+        $this->certificate = array($path, $password);
+
+        return this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setConnectTimeout($connectTimeout)
+    {
+        $this->connectTimeout = $connectTimeout;
+
+        return this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->headers = $headers;
+
+        return this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setProxy($url)
+    {
+        $this->proxy = $url;
+
+        return this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setSslCertificateVerify($verify)
+    {
+        $this->verifyCertificate = $verify;
+
+        return this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+
+        return this;
+    }
+
 }
--- src/Osm/MetricCollector/HttpClientInterface.php
+++ src/Osm/MetricCollector/HttpClientInterface.php
@@ -13,11 +13,75 @@ interface HttpClientInterface
     /**
      * Sends request and returns body.
      *
-     * @param  string $url
-     * @param  array  $headers
-     * @return string
+     * @param string $url Url of HTTP request
+     *
+     * @return string Content of HTTP response body
+     *
      * @throws HttpResponseException
      */
-    public function send($url, array $headers = array());
+    public function send($url);
 
+    /**
+     * Sets HTTP headers.
+     *
+     * @param array $headers Associative array of headers to add to the request
+     *
+     * @return HttpClientInterface
+     */
+    public function setHeaders(array $headers);
+
+    /**
+     * Sets basic HTTP authentication headers.
+     *
+     * @param array $authentication Array of HTTP authentication parameters to use with the request
+     *
+     * @return HttpClientInterface
+     */
+    public function setAuthentication(array $authentication);
+
+    /**
+     * Sets request timeout.
+     *
+     * @param float $timeout Timeout of the request in seconds. Use 0 to wait indefinitely
+     *
+     * @return HttpClientInterface
+     */
+    public function setTimeout($timeout);
+
+    /**
+     * Sets connection timeout.
+     *
+     * @param float $connectTimeout Number of seconds to wait while trying to connect to a server. Use 0 to wait indefinitely
+     *
+     * @return HttpClientInterface
+     */
+    public function setConnectTimeout($connectTimeout);
+
+    /**
+     * Sets SSL certificate verification behavior of a request.
+     *
+     * @param boolean|string $verify Boolean value for enabling verification or string of path to a CA bundle
+     *
+     * @return HttpClientInterface
+     */
+    public function setSslCertificateVerify($verify);
+
+    /**
+     * Sets client side PEM certificate option.
+     *
+     * @param string $path     Path to a file containing a PEM formatted client side certificate
+     * @param string $password Password of certificate if required
+     *
+     * @return HttpClientInterface
+     */
+    public function setCertificate($path, $password = null);
+
+    /**
+     * Sets HTTP proxy url for all protocols.
+     *
+     * @param string $url URL of HTTP proxy
+     *
+     * @return HttpClientInterface
+     */
+    public function setProxy($url);
 }
